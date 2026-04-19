7d74868aa55e7c56045391bd1f02e0ad8acbf495
ana0191-wq <ana0191@gmail.com>
2026-04-18 22:29:31 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+    <BrowserRouter basename="/pedidosbrillitos/">
         <AppRoutes />
       </BrowserRouter>
     </TooltipProvider>
